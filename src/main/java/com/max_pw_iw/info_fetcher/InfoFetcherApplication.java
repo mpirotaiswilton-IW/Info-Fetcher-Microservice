507a377261f5d1ef2daa4250a21a01cd2d7a6d82
mpirotaiswilton-IW <max.pirotais-wilton@integration.works>
2024-01-16 13:48:08 +1300

    Initial Springboot app added

---
+++ src/main/java/com/max_pw_iw/info_fetcher/InfoFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.max_pw_iw.info_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoFetcherApplication.class, args);
+	}
+
+}
