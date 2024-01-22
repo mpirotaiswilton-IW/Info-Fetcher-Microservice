dfcd458da55daeaaecfc456788629c344d62581d
mpirotaiswilton-IW <max.pirotais-wilton@integration.works>
2024-01-23 10:28:39 +1300

    Attempted fix for init-service container

--- kong/init_services.sh
+++ kong/init_services.sh
@@ -4,24 +4,25 @@ correct=200
 
 while [ "$result" -ne "$correct" ]
 do
+    printf "attempting connection to Kong Admin API... \n\n"
     result=`curl -s -o /dev/null -w "%{http_code}" http://kong-gateway:8001/`
     sleep 1
 done
 
-echo "Connection to Kong Gateway established"
+printf "Connection to Kong Gateway established \n"
 
-echo "Setting up PeopleGet service..."
+printf "\nSetting up PeopleGet service...\n\n"
 
 curl -i -X POST \
 --url http://kong-gateway:8001/services/ \
 --data name=PeoplePerson-Get \
 --data 'url=http://microservice:8080/person/'
 
-echo "Setting up PeopleGet service routes..."
+printf "\n\nSetting up PeopleGet service routes...\n\n"
 
 curl -i -X POST \
 --url http://kong-gateway:8001/services/PeoplePerson-Get/routes/ \
     -H "Content-Type: application/json" \
     -d '{"name": "PeoplePerson-GetRoute", "methods":["GET"], "hosts":["localhost"], "paths":["/person"]}'
 
-echo "Set-up complete"
+printf "\n\nSet-up complete\n"
